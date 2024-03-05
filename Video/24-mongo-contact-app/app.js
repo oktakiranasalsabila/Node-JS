d9c917c2b6db1160d70709a14a9c7b217cdb934f
oktakiranasalsabila <oktakiranasalsabila@gmail.com>
2024-03-05 15:38:29 +0700

    Node_JS

--- Video/24-mongo-contact-app/app.js
+++ Video/24-mongo-contact-app/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
+const { body, validationResult, check } = require('express-validator');
+const methodOverride=require('method-override');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
 const flash = require('connect-flash');
@@ -7,6 +9,9 @@ require('./utils/db');
 const Contact = require('./model/contact');
 const app = express();
 const port = 3000;
+
+//setup method override
+app.use(methodOverride('_method'));
 
 //setup ejs
 app.set('view engine', 'ejs');
@@ -69,6 +74,58 @@ app.get('/contact', async (req, res) => {
     });
 
 });
+
+//halaman form tambah data
+app.get('/contact/add', (req, res) => {
+    res.render('add-contact', {
+        title: 'Form Tambah Data Contact',
+        layout: 'layouts/main-layout',
+
+    });
+});
+
+// proses tambah data contact
+app.post('/contact', [
+    body('nama').custom(async (value) => {
+        const duplikat = await Contact.findOne({ nama: value });
+        if (duplikat) {
+            throw new Error('Nama sudah digunakan');
+        }
+        return true
+    }),
+    check('email', 'Email Tidak Valid!').isEmail(),
+    check('nohp', 'Nohp Tidak Valid!').isMobilePhone('id-ID'),
+], (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        res.render('add-contact', {
+            title: 'Form tambah data Contact',
+            layout: 'layouts/main-layout',
+            errors: errors.array(),
+        });
+    } else {
+        Contact.insertMany(req.body, (error, result) => {
+            //kirimkan flash message
+            req.flash('msg', 'Data berhasil ditambahkan');
+            res.redirect('/contact');
+        });
+    }
+});
+
+// //proses delete contact
+// app.get('/contact/delete/:nama', async (req, res) => {
+//     const contact = await Contact.findOne({ nama: req.params.nama });
+//     if (!contact) {
+//         res.status(404);
+//         res.send('<h1>404</h1>');
+//     } else {
+//         Contact.deleteOne({ _id:contact._id }).then((result) => { 
+//             req.flash('msg', 'Data contact berhasil dihapus');
+//             res.redirect('/contact');
+//         });
+//     }
+// });
+app.delete('/contact',(req,res)=>{})
 
 //halaman detail contact
 app.get('/contact/:nama', async (req, res) => {
